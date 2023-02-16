ad629c6bc8ee3fe17baf95730dd2a950de13d4d2
Baxket <richardablorh7@gmial.com>
2023-02-16 15:28:48 -0800

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,9 @@
 /**
 *main - Entry point 
 *Description: 'outpus a sentence with puts'
-*Return: Always 0 (Success)
-*/
+*Return: Always 0 (Success) */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return 0;
+return (0*1);
 }
