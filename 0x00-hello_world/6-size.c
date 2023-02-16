7e9b7f246e5c363bdcc353911518741a71f1d321
Baxket <richardablorh7@gmial.com>
2023-02-16 15:57:28 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'the program's description'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+pritnf("SIze of a char: $d byte(s)\n", sizeof(char));
+pritnf("SIze of a int: $d byte(s)\n", sizeof(int));
+pritnf("SIze of a long int: $d byte(s)\n", sizeof(long int));
+pritnf("SIze of a long long int: $d byte(s)\n", sizeof(long long int));
+pritnf("SIze of a float: $d byte(s)\n", sizeof(float));
+return (0);
+}
