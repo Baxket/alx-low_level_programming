476133d8f9c8cf23c81e9d2efee5ce7fc5ea14dc
Baxket <richardablorh7@gmial.com>
2023-02-16 16:46:01 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
 int main(void)
 {
 char st[] = "2015-10-19";
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, %s", st);
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, %s\n", st);
 return (1);
 }
