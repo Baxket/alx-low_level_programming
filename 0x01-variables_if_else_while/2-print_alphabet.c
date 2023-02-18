cdb000912eeca4e72cf6543aa8a8faf2f3dc2407
Baxket <richardablorh7@gmial.com>
2023-02-17 18:03:16 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: 'outpus a sentence with puts'
+*Return: Always 0(Success)
+*/
+int main(void)
+{
+char n = 'a';
+while (n <= 'z')
+{
+putchar(n);
+n++;
+}
+return (0);
+}
