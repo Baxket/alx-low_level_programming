200325ce72be4c7e0717e978f8f3e6f89fecbc5d
Baxket <richardablorh7@gmial.com>
2023-02-17 18:26:28 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: 'outpus a sentence with puts'
+*Return: Always 0(Success)
+*/
+int main(void)
+{
+int n = 0;
+for (n ; n < 10 ; n++)
+{
+printf("%d" , n);
+}
+printf("\n");
+return (0);
+}
