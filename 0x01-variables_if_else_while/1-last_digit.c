2df2a176bea6587c85dea11684499798d96be772
Baxket <richardablorh7@gmial.com>
2023-02-17 17:44:45 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ if ((n % 10) > 5)
 {
 printf("%s %d is %d and is greater than 5 \n", c, n, (n % 10));
 }
-else if ((n % 10) < 6 && ld != 0)
+else if ((n % 10) < 6 && (n % 10) != 0)
 {
 printf("%s %d is %d and is less than 6\n", c, n, (n % 10));
 }
