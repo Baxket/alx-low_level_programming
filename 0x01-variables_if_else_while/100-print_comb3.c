95efbe213e11a5fcf6a5e49d7ff440d1abb3c75b
Baxket <richardablorh7@gmial.com>
2023-02-19 15:52:23 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,17 +6,16 @@
 */
 int main(void)
 {
-int n;
-int j;
+int n, j;
 for (n = 0 ; n < 10 ; n++)
 {
-for (j = n+1 ; j < 10 ; j++)
+for (j = n + 1 ; j < 10 ; j++)
 {
 if (n != j)
 {
 putchar(48 + n);
 putchar(48 + j);
-if (n < 9)
+if (j < 10)
 {
 putchar(',');
 putchar(' ');
