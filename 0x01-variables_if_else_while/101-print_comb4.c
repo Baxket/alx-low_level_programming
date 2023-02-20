fb7abb7ff0e17e80921886bc140973ac654c3ce8
Baxket <richardablorh7@gmial.com>
2023-02-19 16:10:17 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,14 +6,14 @@
 */
 int main(void)
 {
-int n, j;
+int n, j, k;
 for (n = 0 ; n < 10 ; n++)
 {
 for (j = n + 1 ; j < 10 ; j++)
 {
 for (k = n + 2 ; k < 10 ; k++)
 {
-if (n != j)
+if (n != j && n != k)
 {
 putchar(48 + n);
 putchar(48 + j);
