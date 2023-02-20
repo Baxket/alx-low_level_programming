7d766f75ae68b2119a2be77de6399fcdb7200f10
Baxket <richardablorh7@gmial.com>
2023-02-19 17:18:07 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: 'outpus a sentence with puts'
+*Return: Always 0(Success)
+*/
+int main(void)
+{
+int n, j, p, q;
+for (p = 0; p < 10; p++)
+{
+for (q = 0; q < 10; q++)
+{
+for(i = 0 ; i < 10 ; i++)
+{
+for(k = q + 1 ; k < 10 ; k++
+{
+putchar(48 + p);
+putchar(48 + q);
+putchar(' ');
+putchar(48 + i);
+putchar(48 + k);
+putchar(',');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
