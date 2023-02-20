249ba251e4d2123feac52ad71808392f0aa70ead
Baxket <richardablorh7@gmial.com>
2023-02-19 17:20:26 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,14 +6,14 @@
 */
 int main(void)
 {
-int n, j, p, q;
+int i, k, p, q;
 for (p = 0; p < 10; p++)
 {
 for (q = 0; q < 10; q++)
 {
 for(i = 0 ; i < 10 ; i++)
 {
-for(k = q + 1 ; k < 10 ; k++
+for(k = q + 1 ; k < 10 ; k++)
 {
 putchar(48 + p);
 putchar(48 + q);
