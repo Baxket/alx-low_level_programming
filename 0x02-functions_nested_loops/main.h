867f90edc50ea35ce0aac0a771ec2410e5db52ae
Baxket <richardablorh7@gmial.com>
2023-02-21 15:22:29 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,21 @@
-/*
-Main.h
-Written by Matthew Fisher
-
-Main.h is included by all source files and includes every header file in the correct order.
+#include <unistd.h>
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
 */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+void print_alphabet(void)
+{
+char a = 'a';
+for (a = 'a' ; a <= 'z' ; a ++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
