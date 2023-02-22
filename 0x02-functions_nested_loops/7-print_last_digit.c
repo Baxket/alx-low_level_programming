bbb601dbed2ee3304e04b260b18a1ea6fb84f487
Baxket <richardablorh7@gmial.com>
2023-02-22 12:41:01 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,9 +9,9 @@ int print_last_digit(int c)
 {
 if (c < 0)
 {
-int last = -c % 10;
-_putchar('0' + last);
-return (last);
+int last = c % 10;
+_putchar('0' + -last);
+return (-last);
 }
 _putchar('0' + c % 10);
 return (c % 10);
