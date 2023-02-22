07680cd7ff01ba13ce9f1111a847a0a3702e70c0
Baxket <richardablorh7@gmial.com>
2023-02-21 16:20:57 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,2 @@
 int _putchar(char c);
 void print_alphabet(void);
-/**
-*print_alphabet - prints all lowercase alphabets
-*description: 'prints alphabets'
-*Return: void
-*/
-void print_alphabet(void)
-{
-char a = 'a';
-for (a = 'a' ; a <= 'z' ; a++)
-{
-_putchar(a);
-}
-_putchar('\n');
-}
