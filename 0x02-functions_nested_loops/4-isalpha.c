d200781426a6479731aa10e6aaa6baa235ead938
Baxket <richardablorh7@gmial.com>
2023-02-22 11:12:40 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,12 @@
 /**
 *_isalpha - checks if input is an alphabet
 *@c: it is an intger
-*
-*Description: checks if parameter c is an aplphabet and returns 0 or 1'
-*Returns 0
+*description: checks if parameter c is an aplphabet and returns 0 or 1'
+*Return: 0
 */
 int _isalpha(int c)
 {
-if((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
 return (1);
 }
