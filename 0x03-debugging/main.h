338f2b69358fcdee79ce22a31e7d535de1641e54
Baxket <richardablorh7@gmial.com>
2023-02-23 16:36:18 -0800

    3-print_remaining_days.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,6 +2,7 @@
 #define HEADER_FILE
 #include <stdio.h>
 void postitive_or_negative(int i);
-
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 int largest_number(int a, int b, int c);
 #endif
