83cc8b4f53e0fd04570a25c67c6621e29a449ddf
Baxket <richardablorh7@gmial.com>
2023-02-24 08:40:44 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,14 +12,14 @@ for (a = 1; a <= 10; a++)
 {
 	for (b = 0; b <= 14; b++)
 	{
-		if (ans / 10)
+		if (b / 10)
 		{
-			_putchar('0' + ans / 10);
-			_putchar('0' + ans % 10);
+			_putchar('0' + b / 10);
+			_putchar('0' + b % 10);
 		}
 		else
 		{
-			_putchar(48 + b);
+			_putchar('0' + b);
 		}
 	}
 _putchar('\n');
