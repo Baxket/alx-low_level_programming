3510e8b6921b7e965e808f13a1b10b9a8abee6f6
Baxket <richardablorh7@gmial.com>
2023-02-24 06:58:10 -0800

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -8,13 +8,20 @@
 */
 int _isdigit(int c)
 {
-if (c >= 0 && c <= 9)
+if (c >= 'a' && c <= 'z')
 {
 return (0);
+}
+else
+{
+if (c >= 0 && c <= 9)
+{
+return (1);
 }
 else if (c % 10 >= 0 && c % 10 <= 9)
 {
 return (0);
+}
 }
 return (0);
 }
