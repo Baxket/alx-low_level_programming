7b5a6b2e6d842cddbbab9821c630c4078d566ca4
Baxket <richardablorh7@gmial.com>
2023-02-24 14:53:42 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,11 +8,12 @@
 */
 void print_number(int n)
 {
-	if (n < 0) {
-		_putchar('-');
-		n = -n;
-	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar('0' + n % 10);
+unsigned int a = n;
+if (n < 0) {
+_putchar('-');
+a = -a;
+}
+if (a / 10)
+print_number(a / 10);
+_putchar('0' + a % 10);
 }
