b5ed9b675b12280ba7ddd5150d2793c536fc0eec
Baxket <richardablorh7@gmial.com>
2023-02-27 14:25:04 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*swap_int - a void function
+*@a: it is a pointer
+*@b: it is a pointer
+*
+*Description: swaps two pointers
+*Return: 0
+*/
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = temp;
+}
