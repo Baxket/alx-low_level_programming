dd7cbf188159f763fc9a1b11cf2fb3a8b084b88c
Baxket <richardablorh7@gmial.com>
2023-02-28 14:06:00 -0800

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*_strlen - a function
+*@s: it is a pointer
+*
+*Description: prints length of a string
+*Return: 0
+*/
+void print_array(int *a, int n)
+{
+	int c;
+
+	for (c = 0; c < n; c++)
+	{
+		if (x < n - 1)
+		{
+		printf("%d, ", a[x]);
+		}
+		else
+		{
+		printf("%d", a[x]);
+		}
+	}
+printf("\n");
+}
