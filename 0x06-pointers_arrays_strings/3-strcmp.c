faf748748c7ba0a955f4bfdc92892bfcd2976bdf
Baxket <richardablorh7@gmial.com>
2023-03-02 07:22:18 -0800

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*_strcmp - a function
+*@s1: it is a pointer
+*@s2: it is a pointer
+*Description: Compares two strings
+*Return: 0
+*/
+int _strcmp(char *s1, char *s2)
+{
+	for ( ; *s1 && *s2 ; )
+	{
+	if (*s1 != *s2)
+	{
+		return (*s1 - *s2);
+	}
+		*s1++;
+		*s2++;
+	}
+	return (0);
+}
