e68b9a19358a84169d03d836cc761282594e7e88
Baxket <richardablorh7@gmial.com>
2023-03-02 08:35:54 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
-*_strcat - a function
-*@dest: it is a pointer
-*@src: it is a pointer
-*Description:Concatenate two strings
+*string_toupper - a function
+*@s: it is a pointer
+*Description: it capitalizes a string
 *Return: 0
 */
 char *string_toupper(char *s)
