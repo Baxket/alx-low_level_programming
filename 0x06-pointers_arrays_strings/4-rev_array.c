ec1939a766646531b64ab73a0713d7c4a2b50f27
Baxket <richardablorh7@gmial.com>
2023-03-02 10:18:30 -0800

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,10 +10,10 @@ void reverse_array(int *a, int n)
 {
 	int i, d;
 	
-	for (i = 0; i < n--; i++)
+	for (i = 0; i < (n - 1)/2; i++)
 	{
 		d = a[i];
-		a[i] = a[n];
-		a[n] = d;
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = d;
 	}
 }
