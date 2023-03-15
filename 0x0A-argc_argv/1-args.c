ef8c400f39066a0e1c5a39a9bacf8760ac45c1c0
Baxket <richardablorh7@gmial.com>
2023-03-15 16:50:10 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
 *main - Entry point
+*@argc: an integer
+*@argv: a pointer
 *Description: 'outpus a sentence with puts'
 *Return: Always 0(Success)
 */
 int main(int argc, char **argv)
 {
-	
 
 	printf("%d\n", argc - 1);
 	return (0);
