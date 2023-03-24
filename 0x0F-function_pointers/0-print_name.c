afedabcf92fab7886ba4cb66e5729d170379f693
Baxket <richardablorh7@gmial.com>
2023-03-24 09:55:27 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,11 @@
 #include "function_pointers.h"
+#include <stddef.h>
 /**
  * print_name - a function that prints a name.
  * @name: the name given
  * @f: function of name
  *
- * Rteurn: void
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
